5d4a72fcca526e4bec06dc4f6812112ac7cb0b7d
JM <jmjanzen@gmail.com>
2017-01-01 18:14:24 -0500

    Fix import dir and use better test in main

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,12 +1,16 @@
-#include <stdio.h>
-#include <menu.h>
+#include <iostream>
+#include "game.h"
 
 int main() {
-    printf("main\n");
 
-    if (init() == 0) {
-        printf("good\n");
+    vec2ui v1 = {1,0};
+    vec2ui v2 = {2,0};
+
+    if (v1.x > v2.x) {
+        std::cout << v1.x << " is greater than " << v2.x << std::endl;
     } else {
-        printf("bad\n");
+        std::cout << v1.x << " is not greater than " << v2.x << std::endl;
     }
+
+
 }
